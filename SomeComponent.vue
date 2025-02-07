2fb86571c68892e1954ef3fe5c5b37a764a596e0
BugHunter-X <askzenai@gmail.com>
2025-02-07 19:48:04 +0100

    Add SomeComponent.vue file

---
+++ SomeComponent.vue
@@ -0,0 +1,23 @@
+```javascript
+// SomeTailwind.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'theme-color': '#333',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+};
+```
+```html
+<!-- SomeComponent.vue -->
+<template>
+  <div class="bg-theme-color">
+    This is some text
+  </div>
+</template>
+```
